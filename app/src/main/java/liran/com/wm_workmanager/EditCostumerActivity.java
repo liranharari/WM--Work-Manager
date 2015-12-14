92efe209967845ca145d1bf0737389296d502ff6
liran <liran.94.h@gmail.com>
2015-12-14 18:58:54 +0200

    add edit costumer page

---
+++ app/src/main/java/liran/com/wm_workmanager/EditCostumerActivity.java
@@ -0,0 +1,13 @@
+package liran.com.wm_workmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditCostumerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_costumer);
+    }
+}
