9aa5d97364d3b23ef791e91e6191184b508ec00b
liran <liran.94.h@gmail.com>
2015-12-31 14:40:30 +0200

    add time dialog to make reminder page

--- app/src/main/java/liran/com/wm_workmanager/MakeReminderActivity.java
+++ app/src/main/java/liran/com/wm_workmanager/MakeReminderActivity.java
@@ -3,6 +3,7 @@ package liran.com.wm_workmanager;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,8 +21,10 @@ import java.util.List;
 public class MakeReminderActivity extends Activity {
 
     private Spinner spinCostumer;
-    private int remYear, remMonth, remDay;
-    static final int DATE_DIALOG_ID=0;
+    private int remYear, remMonth, remDay, remHour, remMinute;
+    static final int DATE_DIALOG_ID=0, TIME_DIALOG_ID=1;
+
+    private Button btnSaveReminder;
 
     private Button btnDate, btnTime;
 
@@ -35,12 +39,25 @@ public class MakeReminderActivity extends Activity {
         remYear=cal.get(Calendar.YEAR);
         remMonth=cal.get(Calendar.MONTH);
         remDay=cal.get(Calendar.DAY_OF_MONTH);
+        remHour = cal.get(Calendar.HOUR_OF_DAY);
+        remMinute = cal.get(Calendar.MINUTE);
+
 
         spinCostumer = (Spinner) findViewById(R.id.spinner_costumer);
+
+        btnSaveReminder=(Button) findViewById(R.id.btn_save_reminder);
 
         createSpinCostumer();
 
         showDialogOnButtonClick();
+
+        btnSaveReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "reminder saved to the "+remDay+"/"+remMonth+"/"+ remDay+"\n"+
+                                        "at " +remHour + ":" + remMinute , Toast.LENGTH_LONG).show();
+            }
+        });
 
 
 
@@ -49,11 +66,19 @@ public class MakeReminderActivity extends Activity {
 
     public void showDialogOnButtonClick(){
         btnDate= (Button) findViewById(R.id.btn_date);
+        btnTime =(Button) findViewById(R.id.btn_time);
 
         btnDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(TIME_DIALOG_ID);
             }
         });
     }
@@ -63,6 +88,8 @@ public class MakeReminderActivity extends Activity {
     {
         if(id==DATE_DIALOG_ID)
             return new DatePickerDialog(this, dpickerListener, remYear, remMonth, remDay);
+        if(id==TIME_DIALOG_ID)
+            return new TimePickerDialog(this, tpickerListener,remHour, remMinute, true);
         return null;
     }
 
@@ -74,7 +101,24 @@ public class MakeReminderActivity extends Activity {
             remYear=year;
             remMonth=monthOfYear +1;
             remDay=dayOfMonth;
-            btnDate.setText(remDay+"/"+remMonth+"/"+ remDay);
+            btnDate.setTextColor(getResources().getColor(R.color.cyan));
+            btnDate.setText(remDay+"/"+remMonth+"/"+ remYear);
+
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener tpickerListener
+            =new TimePickerDialog.OnTimeSetListener(){
+
+        @Override
+        public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+            remHour = selectedHour;
+            remMinute = selectedMinute;
+            btnTime.setTextColor(getResources().getColor(R.color.cyan));
+            btnTime.setText(remHour + ":" + remMinute);
+
+
+
 
         }
     };
