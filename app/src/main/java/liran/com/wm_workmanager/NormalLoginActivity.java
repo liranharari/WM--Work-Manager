125d98b59952e87ff4249d133780d54fe1a5e572
liran <liran.94.h@gmail.com>
2016-01-06 18:57:44 +0200

    begin normal login page

---
+++ app/src/main/java/liran/com/wm_workmanager/NormalLoginActivity.java
@@ -0,0 +1,20 @@
+package liran.com.wm_workmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NormalLoginActivity extends Activity {
+
+    private EditText editCode;
+    private Button
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_login);
+
+
+    }
+}
