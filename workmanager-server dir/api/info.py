03407ffb1769f37f3bbdafe132694b125b286876
liran <liran.94.h@gmail.com>
2016-03-07 12:14:00 +0200

    add get info about user

--- workmanager-server dir/api/info.py
+++ workmanager-server dir/api/info.py
@@ -5,24 +5,20 @@ import json
 from models.user import User
 
 
-class logInManager(webapp2.RequestHandler):
+class getUserInfo(webapp2.RequestHandler):
 	def get(self):
-		template_params={}
+  		template_params={}
 		mail=self.request.get('mail')
-		password=self.request.get('password')
 		
 		user= User.query(User.mail == mail).get()
 		if not user:
 			self.error(403)
-			self.response.write('Wrong username')
+			self.response.write('Error')
 			return
-		if not user.checkPassword(password):
-			self.error(403)
-			self.response.write('Wrong password')
-			return
-		template_params['status']="OK"
-		template_params['user']=user.mail
-		template_params['type']="manager"
+		
+		template_params['code']=user.code
+		template_params['managerPricing']=user.managerPricing
+		template_params['workerPricing']=user.workerPricing
 		self.response.write(json.dumps(template_params))
 
 class logInWorker(webapp2.RequestHandler):
@@ -41,6 +37,5 @@ class logInWorker(webapp2.RequestHandler):
 		self.response.write(json.dumps(template_params))
 
 app = webapp2.WSGIApplication([
-	('/api/loginManager',logInManager),
-	('/api/loginCode', logInWorker)
+	('/api/getuserinfo',getUserInfo)
 ], debug=True)
